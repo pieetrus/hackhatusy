9d7177bfdcab5f2558c1c19d8db1e185f10afee3
Paweł Ptak <pawel.ptak@atos.net>
2021-11-19 23:52:40 +0100

    web scraping work in progress

---
+++ web_scraping_pawel/main.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service("C:\pawel_workspace\web_scraping\chromedriver\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+URL = "https://www.meetup.com/pl-PL/find/?location=pl--Wroclaw&source=EVENTS&eventType=inPerson"
+
+def get_html(url):
+    driver.get(url)
+    content = driver.page_source
+    # print(content)'
+    soup = BeautifulSoup(content, features='html.parser')
+    return soup
+
+# print(list(body.children))
+
+wroclaw_events_content = get_html(URL)
+event_cards = wroclaw_events_content.find_all('a', id='event-card-in-search-results')
+event_links = []
+for card in event_cards:
+    event_link = card.get('href')
+    event_links.append(event_link)
+
+sample_event = ''
+for link in event_links:
+    # print(link)
+    if '282122167' in link:
+        sample_event = link
+        # print(sample_event)
+
+event_content = get_html(sample_event)
+
+title = event_content.find('title').string # event title
+dates = event_content.find_all('span', class_="eventTimeDisplay-startDate")
+date = dates[0].string # event date
+times = event_content.find_all('span', class_="eventTimeDisplay-startDate-time")
+start_time = times[0].string # event start time
+times = event_content.find_all('span', class_="eventTimeDisplay-endDate-partialTime")
+end_time = (times[0].find_all('span')[0]).string # event end time
+time_zone = (times[0].find_all('span')[1]).string # event time zone
+
+driver.close()
