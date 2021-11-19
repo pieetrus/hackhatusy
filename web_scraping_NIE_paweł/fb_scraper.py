e7b6898edef42cd146175af398268198b3924402
wek98 <57508778+wek98@users.noreply.github.com>
2021-11-20 00:34:34 +0100

    next initial commit message

---
+++ web_scraping_NIE_paweł/fb_scraper.py
@@ -0,0 +1,29 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import time
+
+##options
+chrome_options = Options()
+#chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--no-sandbox")
+
+
+s = Service('D:/chromedriver.exe')
+driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
+driver.maximize_window()
+driver.get('https://www.facebook.com/events/search/?q=wroc%C5%82aw')
+time.sleep(2)
+driver.execute_script('window.scrollBy(0,2500)', '')
+#driver.find_element_by_xpath('//*[@id="uc-btn-accept-banner"]').click()
+content = driver.page_source
+
+#response = requests.get('https://www.facebook.com/events/search/?q=wroc%C5%82aw')
+soup = BeautifulSoup(content, 'html.parser')
+aaa = soup.find_all('a')
+for a in aaa:
+    #links = aaa.findAll('href')
+    print(a.get('href'))
