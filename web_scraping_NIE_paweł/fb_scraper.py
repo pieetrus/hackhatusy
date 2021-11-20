6d1b618bcd6cbbb579ed6b3082b87cbea8419d7e
wek98 <57508778+wek98@users.noreply.github.com>
2021-11-20 01:23:02 +0100

    Add files via upload
    facebook get through events

--- web_scraping_NIE_paweł/fb_scraper.py
+++ web_scraping_NIE_paweł/fb_scraper.py
@@ -1,17 +1,19 @@
 import requests
+import bs4
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
+import re
 
 ##options
 chrome_options = Options()
 #chrome_options.add_argument("--headless")
-#chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--no-sandbox")
 
-
-s = Service('./chromedriver.exe')
+facebook_event_link = 'https://www.facebook.com/events/'
+s = Service('D:/chromedriver.exe')
 driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
 driver.maximize_window()
 driver.get('https://www.facebook.com/events/search/?q=wroc%C5%82aw')
@@ -23,6 +25,19 @@ content = driver.page_source
 #response = requests.get('https://www.facebook.com/events/search/?q=wroc%C5%82aw')
 soup = BeautifulSoup(content, 'html.parser')
 aaa = soup.find_all('a')
+event_ids = []
 for a in aaa:
     #links = aaa.findAll('href')
-    print(a.get('href'))
+    event_links = a.get('href')
+    event_ids_tmp = re.findall('[0-9]*', event_links)
+    for event_id in event_ids_tmp:
+        if len(event_id) > 3:
+            print(event_id)
+            event_ids.append(event_id)
+
+for event_id in event_ids:
+    event_link = facebook_event_link + event_id
+    driver.get(event_link)
+    event_content = driver.page_source
+    event_soup = BeautifulSoup(event_content, 'html.parser')
+    
