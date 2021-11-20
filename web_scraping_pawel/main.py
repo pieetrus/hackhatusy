f789466c2503cb711ad275cdfba9f8c2b280b525
Paweł Ptak <pawel.ptak@atos.net>
2021-11-20 02:06:43 +0100

    add coordinates scraping

--- web_scraping_pawel/main.py
+++ web_scraping_pawel/main.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import re
 
-##options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
@@ -34,30 +34,37 @@ for card in event_cards:
 sample_event = ''
 print(len(event_links))
 for link in event_links:
-    # print(link)
-    if '282122167' in link:
-        sample_event = link
-        # print(sample_event)
+    event_content = get_html(link)
 
-event_content = get_html(sample_event)
+    title = event_content.find('title').string  # event title
+    date = event_content.find('span', class_="eventTimeDisplay-startDate").string  # event date
+    start_time = event_content.find('span', class_="eventTimeDisplay-startDate-time").string  # event start time
+    time_list = event_content.find_all('span', class_="eventTimeDisplay-endDate-partialTime")
+    end_time = (time_list[0].find_all('span')[0]).string  # event end time
+    time_zone = (time_list[0].find_all('span')[1]).string  # event time zone
 
-title = event_content.find('title').string  # event title
-date = event_content.find('span', class_="eventTimeDisplay-startDate").string  # event date
-start_time = event_content.find('span', class_="eventTimeDisplay-startDate-time").string  # event start time
-time_list = event_content.find_all('span', class_="eventTimeDisplay-endDate-partialTime")
-end_time = (time_list[0].find_all('span')[0]).string  # event end time
-time_zone = (time_list[0].find_all('span')[1]).string  # event time zone
+    location_list = event_content.find_all('p', class_="wrap--singleLine--truncate")
+    location = location_list[0].string  # event location
 
-location_list = event_content.find_all('p', class_="wrap--singleLine--truncate")
-location = location_list[0].string  # event location
+    district = event_content.find('p', class_="venueDisplay-venue-address").getText()  # event district
 
-district = event_content.find('p', class_="venueDisplay-venue-address").getText()  # event district
+    description = event_content.find('div', class_="event-description runningText").getText()  # event description
 
-description = event_content.find('div', class_="event-description runningText").getText()  # event description
+    gmaps_link = event_content.find('img', class_="venueMap-mapImg span--100").get('src')
+    print(gmaps_link)
 
-coordinates = event_content.find('a', class_="venueMap-mapLink").get('href')
-start_index = coordinates.find('query=') + len('query=')
-end_index = coordinates.find('%')
+    coordinates_pattern = '[0-9][0-9].[0-9]*%2C[0-9][0-9].[0-9]*&'
+    coordinates = re.search(coordinates_pattern, gmaps_link).group(0)
+    latitude = coordinates[:coordinates.find('%')] # event latitude
+    longitude = coordinates[coordinates.find('%')+3:coordinates.find('&')] # event longitude
 
-latitude = coordinates[start_index:end_index]  # event latitude
-longitude = coordinates[end_index + 3:]  # event longitude
+    print('title:', title)
+    print('date:', date)
+    print('start time:', start_time)
+    print('end time:', end_time)
+    print('time zone:', time_zone)
+    print('location:', location)
+    print('district:', district)
+    print('desc:', description)
+    print('x:', latitude)
+    print('y:', longitude)
